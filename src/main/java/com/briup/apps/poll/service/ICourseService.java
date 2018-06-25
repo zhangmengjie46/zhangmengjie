eba65abb1fb9d10db29dd21045c33178e7724f9e
李春雨 <@192.168.15.129>
2018-06-25 11:17:47 +0800

    初始化提交

---
+++ src/main/java/com/briup/apps/poll/service/ICourseService.java
@@ -0,0 +1,19 @@
+package com.briup.apps.poll.service;
+
+import java.util.List;
+
+import com.briup.apps.poll.bean.Course;
+
+public interface ICourseService {
+	List<Course> findAll() throws Exception;
+	
+	List<Course> query(String keywords) throws Exception;
+	
+	void save(Course course) throws Exception;
+	
+	void update(Course course) throws Exception;
+	
+	void deleteById(long id) throws Exception;
+	
+	void batchDelete(List<Long> ids) throws Exception;
+}
